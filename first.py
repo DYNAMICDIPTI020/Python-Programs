e704b2861ac007cfec5af6ef28e5c19ba5bd5d3a
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-08-23 23:40:11 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,2 +1,9 @@
-iq = 91
-print(iq)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num1 < num2:
+    print(f"{num1} is less than {num2}")
+else:
+    print(f"{num1} is equal to {num2}")
